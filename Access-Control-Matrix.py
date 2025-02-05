94ed987d6896c42183ab8e19b58d1670bda2c2b3
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2025-02-04 18:54:20 -0700

    Create Access-Control-Matrix.py

---
+++ Access-Control-Matrix.py
@@ -0,0 +1,9 @@
+# Access Control Matrix (ACM) for predifined subjects and objects and rights
+# @author Gabriel Tinsley
+ACM = {}
+
+# Predifined subjects, objects, and rights
+subjects = ["Alice", "Bob", "Charlie", "Eve", "Mallory"]
+objects = ["File1", "File2", "Printer", "Server1", "Database"]
+rights = ["read", "write", "execute", "own"]
+
